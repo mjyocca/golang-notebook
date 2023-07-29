002ee18fcddfeaad3359eb942d7676e1a1309efe
Michael Yocca <mjyocca@gmail.com>
2023-07-28 17:48:41 -0700

    Update next and nextra configs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  // mdxOptions: {
+  //   rehypePrettyCodeOptions: {
+  //     theme: 'github-dark'
+  //   }
+  // }
 })
 
-module.exports = withNextra({
-  output: "export",
-  basePath: "/golang-notebook",
-  images: {
-    unoptimized: true,
-  },
-})
+let nextProps = {}
+
+if (process.env.NODE_ENV !== 'development') {
+  nextProps = {
+    output: "export",
+    basePath: "/golang-notebook",
+    images: {
+      unoptimized: true,
+    },
+  }
+}
+
+module.exports = withNextra(nextProps)
--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,10 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Golang Notebook</span>,
-  // project: {
-  //   link: 'https://github.com/shuding/nextra-docs-template',
-  // },
-  // chat: {
-  //   link: 'https://discord.com',
-  // },
-  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  project: {
+    link: 'https://github.com/mjyocca/golang-notebook'
+  },
+  docsRepositoryBase: 'https://github.com/mjyocca/golang-notebook',
   footer: {
     text: 'Golang Notebook',
   },
